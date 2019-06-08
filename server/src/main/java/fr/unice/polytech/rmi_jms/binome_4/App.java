92dcb3f989d0a16c5708142e235276b60555f72b
Gregoire Peltier <pg501842@etu.unice.fr>
2019-06-08 15:03:33 +0200

    Initial Client Server maven project

---
+++ server/src/main/java/fr/unice/polytech/rmi_jms/binome_4/App.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.rmi_jms.binome_4a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
