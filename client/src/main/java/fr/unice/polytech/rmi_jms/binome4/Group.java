5b374ebb034a0cc116699bfce830ec29d30e423a
Gregoire Peltier <pg501842@etu.unice.fr>
2019-06-08 15:23:33 +0200

    deleted intelij files

---
+++ client/src/main/java/fr/unice/polytech/rmi_jms/binome4/Group.java
@@ -0,0 +1,9 @@
+package fr.unice.polytech.rmi_jms.binome4;
+
+import java.util.List;
+
+public class Group {
+    String name;
+    List<String> subscribed_user;
+
+}
---
+++ server/src/test/java/fr/unice/polytech/rmi_jms/binome_4a/AppTest.java
@@ -0,0 +1,20 @@
+package fr.unice.polytech.rmi_jms.binome_4a;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
